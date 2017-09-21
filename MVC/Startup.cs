02bd7bc3d2258f1cc4e4e71a7611d65191f68995
Ahmed Chowdhury <rupock@yahoo.com>
2017-09-20 23:23:15 -0400

    Comit first build

--- MVC/Startup.cs
+++ MVC/Startup.cs
@@ -4,24 +4,37 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using MVC.Data;
 
 namespace MVC
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration,IHostingEnvironment env)
         {
             Configuration = configuration;
-        }
+			var builder = new ConfigurationBuilder()
+			   .SetBasePath(env.ContentRootPath)
+			   .AddJsonFile("appsettings.json", optional: true, reloadOnChange: false);
+			   //.AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
+
+			Configuration = builder.Build();
+		}
 
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+			services.AddDbContext<SchoolContext>(options =>
+			{
+				options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+			});
             services.AddMvc();
+			
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -38,7 +51,7 @@ namespace MVC
             }
 
             app.UseStaticFiles();
-
+	
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
